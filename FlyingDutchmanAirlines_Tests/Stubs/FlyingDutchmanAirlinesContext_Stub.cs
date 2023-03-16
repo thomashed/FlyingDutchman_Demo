ab9b7842b9c893274c24697d196a4164b335925d
Thomas <thomashj93@gmail.com>
2023-03-16 07:33:53 +0100

    Fix condition for proper DB excepetions on AirportRepo

--- FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
+++ FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
@@ -24,14 +24,31 @@ public class FlyingDutchmanAirlinesContext_Stub : FlyingDutchmanAirlinesContext
         {
             throw new Exception("Database Error!");
         }
-
-        // TODO: continue this
-        // if (!airports.Any())
-        // {
-        //     throw new Exception("Database Error!"); 
-        // }
+        
+        if (airports.Any(airport => airport.AirportId != 0))
+        {
+            throw new Exception("Database Error!"); 
+        }
         
         await base.SaveChangesAsync(cancellationToken);
         return 1;
     }
+    
+    // public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) {
+    //     await base.SaveChangesAsync(cancellationToken);
+    //         cancellationToken = default) {
+    //         IEnumerable<EntityEntry> pendingChanges = ChangeTracker.Entries()
+    //             .Where(e => e.State == EntityState.Added);
+    //         IEnumerable<Booking> bookings = pendingChanges
+    //             .Select(e => e.Entity).OfType<Booking>();
+    //         if (bookings.Any(b => b.CustomerId != 1)) {
+    //             throw new Exception("Database Error!");
+    //         }
+    //         IEnumerable<Airport> airports = pendingChanges 
+    //             .Select(e => e.Entity).OfType<Airport>(); 
+    //         if (!airports.Any()) { 
+    //             throw new Exception("Database Error!"); 
+    //         }
+    //         await base.SaveChangesAsync(cancellationToken);
+    //         ret
 }
