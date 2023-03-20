879331bdead589c1f1cf5de5af2bfa0c14f16301
Thomas <thomashj93@gmail.com>
2023-03-20 09:11:21 +0100

    Add tests for BookingService input validations

--- FlyingDutchmanAirlines_Tests/ServiceLayer/BookingServiceTests.cs
+++ FlyingDutchmanAirlines_Tests/ServiceLayer/BookingServiceTests.cs
@@ -35,10 +35,29 @@ public class BookingServiceTests
         mockCustomerRepository.Setup(repository => 
             repository.GetCustomerByName("Leo Tolstoy")).Returns(Task.FromResult(new Customer("Leo Tolstoy")));
         
-        BookingService service = new BookingService(mockBookingRepository.Object, mockCustomerRepository.Object);
+        BookingService service = 
+            new BookingService(mockBookingRepository.Object, mockCustomerRepository.Object);
 
         (bool result, Exception exception) = await service.CreateBooking("Leo Tolstoy", 0);
         Assert.IsTrue(result);
         Assert.IsNull(exception);
+    }
+
+    [TestMethod]
+    [DataRow("", 0)]
+    [DataRow(null, -1)]
+    [DataRow("Galileo Galilei", -1)]
+    public async Task CreateBooking_Failure_InvalidInputArguments(string name, int flightNumber)
+    {
+        Mock<BookingRepository> mockBookingRepository = new Mock<BookingRepository>();
+        Mock<CustomerRepository> mockCustomerRepository = new Mock<CustomerRepository>();
+
+        BookingService service = 
+            new BookingService(mockBookingRepository.Object, mockCustomerRepository.Object);
+
+        (bool result, Exception exception) = await service.CreateBooking(name, flightNumber);
+        
+        Assert.IsFalse(result);
+        Assert.IsNotNull(exception);
     }
 }
