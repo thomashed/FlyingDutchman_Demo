5900fd5eccd67d3ddb596f8fd02915c28780bd1c
Thomas <thomashj93@gmail.com>
2023-03-29 08:31:16 +0200

    add initial route mapping for FLightController

--- FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
+++ FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FlyingDutchmanAirlines.ControllerLayer;
 
+[Route("{controller}")]
 public class FlightController : Controller
 {
     private FlightService _flightService;
@@ -27,6 +28,7 @@ public class FlightController : Controller
         }
     }
 
+    [HttpGet]
     public virtual async Task<IActionResult> GetFlights()
     {
         try
