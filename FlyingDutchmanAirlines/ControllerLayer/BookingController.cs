9f1ae7d829ccd81fc219bda716f79fc83d44d45a
Thomas <thomashj93@gmail.com>
2023-04-03 07:57:38 +0200

    expand return statements of BookingController

--- FlyingDutchmanAirlines/ControllerLayer/BookingController.cs
+++ FlyingDutchmanAirlines/ControllerLayer/BookingController.cs
@@ -1,7 +1,9 @@
 using System.Net;
 using FlyingDutchmanAirlines.ControllerLayer.JsonData;
+using FlyingDutchmanAirlines.Exceptions;
 using FlyingDutchmanAirlines.ServiceLayer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 
 namespace FlyingDutchmanAirlines.ControllerLayer;
 
@@ -15,13 +17,23 @@ public class BookingController : Controller
         _bookingService = bookingService;
     }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateBooking([FromBody] BookingData body)
+    [HttpPost("{flightNumber}")]
+    public async Task<IActionResult> CreateBooking([FromBody] BookingData body, int flightNumber)
     {
 
-        if (ModelState.IsValid)
+        if (ModelState.IsValid && flightNumber.IsPositive())
         {
-            
+            string name = $"{body.FirstName} {body.LastName}";
+            (bool result, Exception exception) = await _bookingService.CreateBooking(name, flightNumber);
+
+            if (result && exception == null)
+            {
+                return StatusCode((int)HttpStatusCode.Created);
+            }
+
+            return exception is CouldNotAddBookingToDatabaseException
+                ? StatusCode((int)HttpStatusCode.NotFound)
+                : StatusCode((int)HttpStatusCode.InternalServerError, exception.Message);
         }
 
         return StatusCode((int)HttpStatusCode.InternalServerError, 
